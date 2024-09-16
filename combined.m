81a5e886c8ff42274e32502da75c03f851745080
Aleksandr Vakulenko <sasha705@mail.ru>
2024-09-16 13:23:43 +0300

    local_time

--- combined.m
+++ combined.m
@@ -5,12 +5,12 @@ clc
 clear
 
 R1 = 100e3;
-R2 = 100e3;
+R2 = 1e3;
 C = 10e-6;
 
-tstep = 0.01;
+tstep = 0.001;
 start = 0;
-stop = 10;
+stop = 10-tstep;
 t = (start:tstep:stop);
 Nt = -(start-stop)/tstep+1;
 
@@ -23,29 +23,47 @@ to = 300;
 first = 0;
 second = 0;
 
+Time_limit = 0.2;
+
 %Кривое создание треугольного импульса
 Uin = t(1 : Nt/2)*k;
 Uin(Nt/2 : Nt) = t(Nt/2 : Nt)*(-k) + 40;
 Uout = zeros(1,Nt);
-
+circ_num_out  =[];
+circuit_num = 1;
+local_time = 0;
 for i = 1 : Nt-1
-    if Uout(i) < Ulim 
-        Uout(i+1) = tstep/(R1*C) * (Uin(i) - Uout(i)) + Uout(i);
-        first = first + 1;
-        
-    elseif Uout(i) > Ulim 
-        for j = i-1 : 300 %Неясно, какая нужна правая граница
-            Uout(j+1) = tstep/(R1*C) * (Uin(j) - Uout(j)) - tstep/(R2*C)*Uout(j) + ... 
-            + Uout(j);   
-        end
-        second = second + 1;
+    time = t(i)
+
+    switch circuit_num
+        case 1
+            Uout(i+1) = tstep/(R1*C) * (Uin(i) - Uout(i)) + Uout(i);
+
+        case 2
+            Uout(i+1) = tstep/(R1*C) * (Uin(i) - Uout(i)) - tstep/(R2*C)*Uout(i) + ...
+                + Uout(i);
     end
+
+    if Uout(i) > Ulim
+        local_time = time;
+        circuit_num = 2;
+    else
+        if time - local_time > Time_limit
+            circuit_num = 1;
+        end
+    end
+
+    circ_num_out(i) = circuit_num;
 end
 
+circ_num_out(end+1) = circ_num_out(end);
+
+figure
 hold on
 plot(t,Uin, 'b')
 yline (Ulim)
 plot(t, Uout, 'r');
+plot(t, circ_num_out, 'k');
 
 
 
