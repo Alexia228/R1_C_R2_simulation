bddfd0c293a07611600df988b305a9d96e51d4d5
Alex <fanway56@gmail.com>
2024-10-30 16:16:16 +0300

    dwm is added

--- test_switching.m
+++ test_switching.m
@@ -1,10 +1,10 @@
 
 addpath(genpath('FE_simulator/'));
 
+
 %%
 
 clc
-
 
 Amp = 150;
 V_step = Amp/250;
@@ -25,37 +25,72 @@ for k = 1:20
     FE(k) = FE_part(Sample);
 end
 
-voltage = [0:V_step:Amp Amp:-V_step:-Amp -Amp:V_step:0];
+% voltage = [0:V_step:Amp Amp:-V_step:-Amp -Amp:V_step:0];
 
 % voltageDWM = [0:V_step:Amp Amp:-V_step:0 0:V_step:Amp ...
 %               Amp:-V_step:-Amp -Amp:V_step:0 0:-V_step:-Amp -Amp:V_step:0];
 % voltage = voltageDWM;
 
-time = linspace(0, T_period , numel(voltage));
-time_step = time(2) - time(1);
+% time = linspace(0, T_period , numel(voltage));
+% time_step = time(2) - time(1);
 
-Current = [];
-for  i = 1:numel(voltage)
-    cur = 0;
-    for k = 1:numel(FE)
-        cur = cur + FE(k).get_p(voltage(i), time_step);
-    end
-    Current(i) = cur/numel(FE);
-end
+waveform_up = voltage_triangle(150,10, 1);
+waveform_down = voltage_triangle(150,10, -1);
 
-Q_int = cumsum(Current)*time_step;
-P_int = (Q_int*1e6)/(Area*100*100);
-Field = (voltage*1e-3)/(Thickness*100);
+[Field_1, P_int_1] = Experiment(Sample, FE, waveform_up);
+[Field_2, P_int_2] = Experiment(Sample, FE, waveform_up);
+[Field_3, P_int_3] = Experiment(Sample, FE, waveform_down);
+[Field_4, P_int_4] = Experiment(Sample, FE, waveform_down);
+Pmax_1 = max(P_int_1);
+Pmin_1 = min(P_int_3);
+Pmax_2 = max(P_int_2);
+Pmin_2 = min(P_int_4);
 
+figure
 hold on
-plot(Field, P_int, '.')
+% plot(Field_1, P_int_1-Pmax_1/2+5.5, '.')
+% plot(Field_2, P_int_2+1.2, '.')
+% plot(Field_3, P_int_3-Pmin_1/2, '.')
+% plot(Field_4, P_int_4-Pmin_2/2, '.')
+
+P_int_p = P_int_1 - P_int_2;
+P_int_n = P_int_3 - P_int_4;
+
+correction_p = (P_int_p(end)-P_int_p(1))/2;
+correction_n = (P_int_n(end)-P_int_n(1))/2;
+
+plot(Field_1, P_int_p-correction_p, '.')
+plot(Field_3, P_int_n-correction_n, '.')
+
 % plot(Field, I)
 xlabel('E, kV/cm')
 ylabel('P, uC/cm^2')
 
 
+function [Field, P_int] = Experiment(Sample, FEset, waveform)
 
+voltage = [waveform.voltage] ;
+time = [waveform.time];
 
+time_step = time(2) - time(1);
+
+Current = [];
+for  i = 1:numel(voltage)
+    cur = 0;
+    for k = 1:numel(FEset)
+        cur = cur + FEset(k).get_p(voltage(i), time_step);
+    end
+    Current(i) = cur/numel(FEset);
+end
+
+Q_int = cumsum(Current)*time_step;
+
+Area = Sample.area;
+Thickness = Sample.thickness;
+P_int = (Q_int*1e6)/(Area*100*100);
+Field = (voltage*1e-3)/(Thickness*100);
+
+end
 
 
 
--- voltage_triangle.m
+++ voltage_triangle.m
@@ -1,13 +1,22 @@
-function [voltage_array, time] = voltage_triangle(amp, period)
-    step = 0.1;
+function [waveform] = voltage_triangle(amp, period, polarity)
+    step = 0.01;
     start_time = 0;
     stop_time = period;
+
     time = (start_time : step : stop_time);
+
     voltage_up = amp/(period / 2)*time;
     voltage_up = voltage_up(1:round(length(time)/2));
     voltage_down = -amp/(period / 2)*time + 2*amp;
     voltage_down = voltage_down(length(voltage_up)+1:length(time));
-    voltage_array = [voltage_up voltage_down];
+    voltage_array = [polarity*voltage_up polarity*voltage_down];
+
+    waveform.voltage = [polarity*voltage_up polarity*voltage_down];
+    waveform.time = time;
+
+    
+
+
 
 
 end
